0bded7bacd5f0d939d883d74da3c6913f254a39d
Shumaim Kiyani <shumaimkiyani@gmail.com>
2025-09-01 21:37:15 +0500

    framer animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,174 @@
-import Image from "next/image";
+"use client";
+
+import { motion, useAnimate, useScroll } from 'framer-motion';
+import { useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
+
+const gridContainerVariants = {
+  hidden: { opacity: 0 },
+  show:{
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+const gridSquareVariants = {
+  hidden: { opacity: 0}, 
+  show: { opacity: 1 }
+}; 
+
+const svgIconVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: "rgba(252, 211, 77, 0)",
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "rgba(252, 211, 77, 1)",
+  }
+};
 
 export default function Home() {
+  const { scrollYProgress: completionProgress } = useScroll();
+
+  const containerRef = useRef(null)
+
+  const isInView = useInView(containerRef, { once: true })
+  const mainControls = useAnimate();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      
+    <div className="flex flex-col gap-10 overflow-x-hidden">
+      <motion.section 
+        variants={gridContainerVariants} 
+        initial="hidden" 
+        animate="show" 
+        className="grid grid-cols-3 p-10 gap-10"
+      >
+        <motion.div
+          variants={gridSquareVariants} 
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        >
+          <motion.div className='w-20 h-20 bg-stone-100 rounded-lg'
+            initial={{ opacity: 0, y: 100}}
+            animate={{ opacity: 1, y: 0}}
+            transition={{ duration: 1, ease: "easeOut", delay: 0.2 }}
+          />
+          <motion.div className='w-20 h-20 bg-stone-100 rounded-full'
+            initial={{ opacity: 0, y: -100}}
+            animate={{ opacity: 1, y: 0}}
+            transition={{ duration: 1, ease: "easeOut", delay: 0.4 }}
+          />
+        </motion.div>
+        <motion.div
+          variants={gridSquareVariants} 
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        >
+          <motion.div
+            className="w-1/3 h-1/3 show-md bg-rose-400"
+            animate={{
+              scale: [1, 2, 2, 1],
+              rotate: [0, 90, 90, 0],
+              borderRadius: ["10%", "10%", "50%", "10%"],
+            }}
+            transition={{ duration: 2, ease: "easeInOut", repeat: Infinity, repeatDelay: 1 }}
+          />
+        </motion.div>
+        <motion.div
+          variants={gridSquareVariants} 
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        >
+          <motion.button
+            whileTap={{ scale: 0.9 }}
+            whileHover={{ 
+              scale: 1.1,
+              backgroundColor: "#d1d5db",
+              color: "black"
+            }}
+            transition={{ bounceDamping: 10, bounceStiffness: 600}}
+            className="bg-emerald-600 w-1/2 py-4 rounded-lg text-2xl text-gray-100 font-light tracking-wide"
+          />
+        </motion.div>
+        <motion.div
+          variants={gridSquareVariants} 
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        >
+          <motion.div
+            className='w-1/3 h-1/3 bg-orange-500 rounded-3xl cursor-grab'
+            drag
+            dragConstraints={{
+              top: -125,
+              right: 125,
+              bottom: 125,
+              left: -125
+            }}
+            dragTransition={{bounceStiffness: 600, bounceDamping: 10 }}
+          />
+        </motion.div>
+        <motion.div
+          variants={gridSquareVariants} 
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        >
+          <motion.div className='w-40 aspect-square bg-gray-50/20 rounded-xl'>
+            <motion.div
+              className='w-full bg-gray-400 rounded-xl h-full origin-bottom'
+              style={{ scaleY: completionProgress }}
+            />
+          </motion.div>
+        </motion.div>
+        <motion.div
+          variants={gridSquareVariants} 
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        >
+          <motion.svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="w-1/2 stroke-amber-500 stroke-[0.5]"
+          >
+            <motion.path
+              d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+              variants={svgIconVariants}
+              initial="hidden"
+              animate="visible"
+              transition={{ 
+                default: { 
+                  duration: 2, 
+                  ease: "easeInOut",
+                  delay: 1,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  repeatDelay: 1
+                 },
+                 fill: {
+                  duration: 2,
+                  ease: "easeIn",
+                  delay: 2,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  repeatDelay: 1
+                 },
+              }}
+          
+            />
+          </motion.svg>
+        </motion.div>
+      </motion.section>
+      <section className="flex flex-col gap-10 mb-10">
+        <h1 className='text-5xl tracking-wide text-slate-100 text-center'>
+          Framer Motion Playground
+        </h1>
+        <p className='text-slate-100 font-thin text-4xl w-1/2 mx-auto'>
+          A playground to test out various features of the Framer Motion library in a Next.js application.
+        </p>
+      </section>
     </div>
   );
 }
